88755fa00d6a36f2c035d326a2207167dc2f8128
almpolat <155629947+almpolat@users.noreply.github.com>
2024-04-23 03:21:27 +0300

    ses

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -6,11 +6,13 @@ public class FPSController : MonoBehaviour
 {
     public float movementSpeed = 5.0f;
     public float mouseSensitivity = 2.0f;
-    public bool isPaused = false;
+    public bool isPaused = false;//Alimert
+    public AudioSource walkAudioSource;//Alimert
 
     private CharacterController characterController;
     private Camera playerCamera;
     private float verticalRotation = 0f;
+    private bool isMoving;//Alimert
 
     void Start()
     {
@@ -18,16 +20,21 @@ public class FPSController : MonoBehaviour
 
         characterController = GetComponent<CharacterController>();
         playerCamera = GetComponentInChildren<Camera>();
+        walkAudioSource = GetComponent<AudioSource>();//Alimert
 
         if (playerCamera == null)
         {
             Debug.LogError("FPS Controller requires a camera child object to function properly.");
         }
+        if (walkAudioSource == null)//Alimert
+        {
+            Debug.LogError("No AudioSource component found on the object.");
+        }
     }
 
     void Update()
     {
-        if (isPaused) return;
+        if (isPaused) return;//Alimert
         // Rotation
         float horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
         transform.Rotate(0, horizontalRotation, 0);
@@ -42,7 +49,17 @@ public class FPSController : MonoBehaviour
 
         Vector3 speed = new Vector3(sideSpeed, 0, forwardSpeed);
         speed = transform.rotation * speed;
-
+        //Alimert
         characterController.SimpleMove(speed);
+        isMoving = characterController.velocity.magnitude > 0;
+
+        if (isMoving && !walkAudioSource.isPlaying)
+        {
+            walkAudioSource.Play();
+        }
+        else if (!isMoving && walkAudioSource.isPlaying)
+        {
+            walkAudioSource.Stop();
+        }
     }
 }
