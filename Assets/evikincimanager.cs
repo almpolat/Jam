fd195eacf3d77881161d30c3888022a5de15475c
almpolat <155629947+almpolat@users.noreply.github.com>
2024-06-06 17:48:43 +0300

    .c

---
+++ Assets/evikincimanager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class evikincimanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dialog.Instance.LoadAndStartDialog("EvÝkiVarýs");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
